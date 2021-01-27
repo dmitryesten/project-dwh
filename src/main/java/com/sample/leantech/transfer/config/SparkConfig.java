9b3ec26e759b3607816683c22d224a66d05fe7b3
andrew <october072017@yandex.ru>
2021-01-27 12:59:20 +0600

    FB-001: Added @ConfigurationProperties.

--- src/main/java/com/sample/leantech/transfer/config/SparkConfig.java
+++ src/main/java/com/sample/leantech/transfer/config/SparkConfig.java
@@ -1,29 +1,31 @@
 package com.sample.leantech.transfer.config;
 
+import lombok.Setter;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Setter
 @Configuration
+@ConfigurationProperties(prefix = "spark")
 public class SparkConfig {
 
-    @Value("${spark.app.name}")
-    private String sparkName;
-    @Value("${spark.master}")
-    private String sparkMaster;
+    private String appname;
+    private String master;
 
     @Bean
     public SparkConf sparkConf(){
         return new SparkConf()
-                .setAppName(sparkName)
-                .setMaster(sparkMaster);
+                .setAppName(appname)
+                .setMaster(master);
     }
 
     @Bean
-    public SparkContext sparkContext() {
-        return new SparkContext(sparkConf());
+    public SparkContext sparkContext(@Autowired SparkConf sparkConf) {
+        return new SparkContext(sparkConf);
     }
 
 }
