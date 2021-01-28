cc5a72af449b01c3c4c7ce34e24e398a430fff8b
andrew <october072017@yandex.ru>
2021-01-28 22:53:16 +0600

    FB-001: Added updated field.

--- src/main/java/com/sample/leantech/transfer/model/dto/request/JiraWorklogDto.java
+++ src/main/java/com/sample/leantech/transfer/model/dto/request/JiraWorklogDto.java
@@ -1,5 +1,6 @@
 package com.sample.leantech.transfer.model.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,8 @@ import java.time.ZonedDateTime;
 public class JiraWorklogDto {
 
     private JiraUserDto updateAuthor;
-    //private ZonedDateTime updated;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    private ZonedDateTime updated;
     private Long timeSpentSeconds;
     private String id;
     private String issueId;
