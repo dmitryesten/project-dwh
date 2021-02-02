73f8ed3ed6745e9c472cc352cd15bfbf30b4838e
Dmitry Denisenko <dmitryesten@gmail.com>
2021-02-02 17:14:50 +0600

    added issue mapper

--- src/main/java/com/sample/leantech/transfer/model/mapper/IssueMapper.java
+++ src/main/java/com/sample/leantech/transfer/model/mapper/IssueMapper.java
@@ -5,6 +5,9 @@ import com.sample.leantech.transfer.model.db.Project;
 import com.sample.leantech.transfer.model.dto.request.JiraIssueDto;
 import com.sample.leantech.transfer.model.dto.request.JiraProjectDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.factory.Mappers;
 
 import java.io.Serializable;
@@ -13,7 +16,18 @@ import java.io.Serializable;
 public interface IssueMapper extends Serializable {
 
     IssueMapper INSTANCE = Mappers.getMapper(IssueMapper.class);
-
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "sourceId", source = "jiraIssueDto.id"),
+            @Mapping(target = "pid", source = "jiraIssueDto.fields.project.id"),
+            @Mapping(target = "type", source = "jiraIssueDto.fields.issuetype.name"),
+            @Mapping(target = "summery", source = "jiraIssueDto.fields.summary"),
+            @Mapping(target = "name", source = "jiraIssueDto.key"),
+            @Mapping(target = "hid",
+                    source = "jiraIssueDto.fields.epic.id",
+                    nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+                    defaultExpression = "java(Integer.parseInt(jiraIssueDto.getFields().getParent().getId() ))") //
+    })
     Issue dtoToModel(JiraIssueDto jiraIssueDto);
 
 }
--- src/test/java/com/sample/leantech/transfer/model/mapper/IssueMapperTest.java
+++ src/test/java/com/sample/leantech/transfer/model/mapper/IssueMapperTest.java
@@ -1,22 +1,17 @@
 package com.sample.leantech.transfer.model.mapper;
 
 import com.sample.leantech.transfer.model.db.Issue;
-import com.sample.leantech.transfer.model.db.Project;
 import com.sample.leantech.transfer.model.dto.request.JiraIssueDto;
-import com.sample.leantech.transfer.model.dto.request.JiraProjectDto;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
 @SpringBootTest
 class IssueMapperTest {
@@ -25,40 +20,125 @@ class IssueMapperTest {
     private JavaSparkContext javaSparkCtx;
 
     @Test
+    public void dtoToModeEpicIssue() {
+        JiraIssueDto jiraIssueDto = getIssueEpicDto();
+        Issue issue = IssueMapper.INSTANCE.dtoToModel(jiraIssueDto);
+        Assertions.assertEquals(jiraIssueDto.getId(), String.valueOf(issue.getSourceId()));
+        Assertions.assertEquals(jiraIssueDto.getKey(), issue.getName());
+        Assertions.assertEquals(
+                jiraIssueDto.getFields().getEpic().getId(),
+                String.valueOf(issue.getHid())
+        );
+    }
+
+    @Test
+    public void dtoToModelParentIssue() {
+        JiraIssueDto jiraIssueDto = getIssueParentDto();
+        Issue issue = IssueMapper.INSTANCE.dtoToModel(jiraIssueDto);
+
+        Assertions.assertEquals(jiraIssueDto.getId(), String.valueOf(issue.getSourceId()));
+        Assertions.assertEquals(jiraIssueDto.getKey(), issue.getName());
+        Assertions.assertEquals(
+                jiraIssueDto.getFields().getProject().getId(),
+                String.valueOf(issue.getPid())
+        );
+        Assertions.assertEquals(
+                jiraIssueDto.getFields().getParent().getId(),
+                String.valueOf(issue.getHid())
+        );
+    }
+
+    @Test
     public void test(){
-        JiraIssueDto jiraIssueDto = getIssueDto();
-        List<JiraIssueDto> listJiraProjectDto = Arrays.asList(jiraIssueDto);
+        JiraIssueDto jiraIssueDto = getIssueDtoNoParentAndEpic();
+        Optional.ofNullable(jiraIssueDto.getFields().getParent()).isPresent();
+    }
 
-        //JavaRDD<JiraIssueDto> javaRddUsers = javaSparkCtx.parallelize(listJiraProjectDto);
-        //Collection<Issue> listProjectConvertedRdd = javaRddUsers.map(IssueMapper.INSTANCE::dtoToModel).collect();
+    public void dtoToModelIssueWithoutEpicAndParent() {
+        JiraIssueDto jiraIssueDto = getIssueDtoNoParentAndEpic();
+        Issue issue = IssueMapper.INSTANCE.dtoToModel(jiraIssueDto);
 
-        Issue issue = IssueMapper.INSTANCE.dtoToModel(getIssueDto());
-        //Assertions.assertNotNull(listProjectConvertedRdd);
-        //Assertions.assertEquals(1, listProjectConvertedRdd.size());
-
+        Assertions.assertEquals(jiraIssueDto.getId(), String.valueOf(issue.getSourceId()));
+        Assertions.assertEquals(jiraIssueDto.getKey(), issue.getName());
+        Assertions.assertEquals(
+                jiraIssueDto.getFields().getProject().getId(),
+                String.valueOf(issue.getPid())
+        );
+        Assertions.assertEquals(
+                jiraIssueDto.getFields().getParent().getId(),
+                String.valueOf(issue.getHid())
+        );
 
     }
 
-    private JiraIssueDto getIssueDto(){
-        JiraIssueDto jiraIssueDto = new JiraIssueDto();
-        JiraIssueDto.Fields fields = new JiraIssueDto.Fields();
-        JiraIssueDto.Fields.Project project = new JiraIssueDto.Fields.Project();
-            project.setId("12-R");
-        fields.setProject(project);
+    @Test
+    public void testRddStreamMapping(){
+        JiraIssueDto jiraIssueDto = getIssueEpicDto();
+        List<JiraIssueDto> listJiraProjectDto = Arrays.asList(jiraIssueDto);
 
-        project.setId("12-sd");
-            jiraIssueDto.setFields(fields);
-            jiraIssueDto.setId("34");
+        JavaRDD<JiraIssueDto> javaRddUsers = javaSparkCtx.parallelize(listJiraProjectDto);
+        Collection<Issue> listProjectConvertedRdd = javaRddUsers.map(IssueMapper.INSTANCE::dtoToModel).collect();
+
+        Assertions.assertNotNull(listProjectConvertedRdd);
+        //Assertions.assertEquals(1, listProjectConvertedRdd.size());
+
+    }
+
+    private JiraIssueDto getIssueEpicDto(){
+        JiraIssueDto jiraIssueDto = new JiraIssueDto();
+            jiraIssueDto.setId("1400");
             jiraIssueDto.setKey("Key-123-test");
+        JiraIssueDto.Fields fields = new JiraIssueDto.Fields();
+            JiraIssueDto.Fields.Project project = new JiraIssueDto.Fields.Project();
+                project.setId(String.valueOf(10000));
+            JiraIssueDto.Fields.IssueType issueType = new JiraIssueDto.Fields.IssueType();
+                issueType.setName("Test-Issue-Type");
+            JiraIssueDto.Fields.Parent epic = new JiraIssueDto.Fields.Parent();
+                epic.setId(String.valueOf(40001));
+        fields.setProject(project);
+        fields.setEpic(epic);
+        fields.setIssuetype(issueType);
+        fields.setSummary("Test-Summery");
+        jiraIssueDto.setFields(fields);
+
         return jiraIssueDto;
     }
 
-    private JiraProjectDto getJiraProjectDto(){
-        JiraProjectDto dto = new JiraProjectDto();
-        dto.setKey(UUID.randomUUID().toString());
-        dto.setName("Test_jira-Project");
-        dto.setProjectTypeKey("Prohect-Test");
-        return dto;
+    private JiraIssueDto getIssueParentDto(){
+        JiraIssueDto jiraIssueDto = new JiraIssueDto();
+        jiraIssueDto.setId("1400");
+        jiraIssueDto.setKey("Key-123-test");
+        JiraIssueDto.Fields fields = new JiraIssueDto.Fields();
+        JiraIssueDto.Fields.Project project = new JiraIssueDto.Fields.Project();
+        project.setId(String.valueOf(10000));
+        JiraIssueDto.Fields.Parent parent = new JiraIssueDto.Fields.Parent();
+            parent.setId(String.valueOf(100015));
+        JiraIssueDto.Fields.IssueType issueType = new JiraIssueDto.Fields.IssueType();
+        issueType.setName("Test-Issue-Type");
+        fields.setProject(project);
+        fields.setParent(parent);
+        fields.setIssuetype(issueType);
+        fields.setSummary("Test-Summery");
+        jiraIssueDto.setFields(fields);
+
+        return jiraIssueDto;
+
+    }
+
+    private JiraIssueDto getIssueDtoNoParentAndEpic(){
+        JiraIssueDto jiraIssueDto = new JiraIssueDto();
+        jiraIssueDto.setId("1400");
+        jiraIssueDto.setKey("Key-123-test");
+        JiraIssueDto.Fields fields = new JiraIssueDto.Fields();
+        JiraIssueDto.Fields.Project project = new JiraIssueDto.Fields.Project();
+        project.setId(String.valueOf(10000));
+        JiraIssueDto.Fields.IssueType issueType = new JiraIssueDto.Fields.IssueType();
+        issueType.setName("Test-Issue-Type");
+        fields.setProject(project);
+        fields.setIssuetype(issueType);
+        fields.setSummary("Test-Summery");
+        jiraIssueDto.setFields(fields);
+        return jiraIssueDto;
     }
 
 }
