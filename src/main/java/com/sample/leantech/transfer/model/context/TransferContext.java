07203871b6a578dfd785893ab30c10ad185d088e
andrew <october072017@yandex.ru>
2021-02-02 19:35:52 +0600

    FB-001: IssueMapper fix.

--- src/main/java/com/sample/leantech/transfer/model/context/TransferContext.java
+++ src/main/java/com/sample/leantech/transfer/model/context/TransferContext.java
@@ -3,8 +3,8 @@ package com.sample.leantech.transfer.model.context;
 import com.sample.leantech.transfer.model.dto.request.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import scala.Serializable;
 
+import java.io.Serializable;
 import java.time.ZonedDateTime;
 import java.util.List;
 
