7095a37e866c934090a38620e3bc2880a12b8eb3
andrew <october072017@yandex.ru>
2021-02-03 13:23:19 +0600

    FB-001: WorklogMapper fix.

--- src/main/java/com/sample/leantech/transfer/model/mapper/WorklogMapper.java
+++ src/main/java/com/sample/leantech/transfer/model/mapper/WorklogMapper.java
@@ -1,10 +1,9 @@
 package com.sample.leantech.transfer.model.mapper;
 
+import com.sample.leantech.transfer.model.context.TransferContext;
 import com.sample.leantech.transfer.model.db.Worklog;
 import com.sample.leantech.transfer.model.dto.request.JiraWorklogDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = ConvertMapper.class)
@@ -14,8 +13,19 @@ public interface WorklogMapper {
 
     @Mappings({
             @Mapping(target = "id", ignore = true),
+            @Mapping(target = "issueId", source = "jiraWorklogDto.issueId"),
             @Mapping(target = "sourceId", source = "jiraWorklogDto.id"),
+            @Mapping(target = "updated", source = "jiraWorklogDto.updated"),
             @Mapping(target = "timeSpentSecond", source = "jiraWorklogDto.timeSpentSeconds")
     })
-    Worklog dtoToModel(JiraWorklogDto jiraWorklogDto);
+    Worklog dtoToModel(JiraWorklogDto jiraWorklogDto, @Context TransferContext ctx);
+
+    @AfterMapping
+    default void afterDtoToModel(JiraWorklogDto source, @MappingTarget Worklog target, @Context TransferContext ctx) {
+        if (ctx != null) {
+            target.setSid(ctx.getSource().getValue());
+            target.setLogId(ctx.getLogId());
+        }
+    }
+
 }
--- src/test/java/com/sample/leantech/transfer/model/mapper/WorklogMapperTest.java
+++ src/test/java/com/sample/leantech/transfer/model/mapper/WorklogMapperTest.java
@@ -1,5 +1,7 @@
 package com.sample.leantech.transfer.model.mapper;
 
+import com.sample.leantech.transfer.model.context.Source;
+import com.sample.leantech.transfer.model.context.TransferContext;
 import com.sample.leantech.transfer.model.db.Worklog;
 import com.sample.leantech.transfer.model.dto.request.JiraUserDto;
 import com.sample.leantech.transfer.model.dto.request.JiraWorklogDto;
@@ -20,37 +22,44 @@ class WorklogMapperTest {
 
     @Test
     public void test(){
-        JiraWorklogDto jiraWorklogDto = getWorklogDto();
-        Worklog worklog = WorklogMapper.INSTANCE.dtoToModel(jiraWorklogDto);
+        JiraWorklogDto jiraWorklogDto = worklogDto();
+        TransferContext ctx = transferContext();
+        Worklog worklog = WorklogMapper.INSTANCE.dtoToModel(jiraWorklogDto, ctx);
         Assertions.assertNotNull(worklog);
         Assertions.assertEquals(jiraWorklogDto.getIssueId(), String.valueOf(worklog.getIssueId()) );
         Assertions.assertEquals(jiraWorklogDto.getUpdated().toInstant(), worklog.getUpdated().toInstant());
     }
 
-    private JiraWorklogDto getWorklogDto(){
+    private JiraWorklogDto worklogDto(){
         JiraWorklogDto jiraWorklogDto = new JiraWorklogDto();
-            jiraWorklogDto.setId("123");
-            jiraWorklogDto.setIssueId("444");
-            jiraWorklogDto.setTimeSpentSeconds(202L);
-            jiraWorklogDto.setUpdateAuthor(getUserDtoFirst());
-            jiraWorklogDto.setUpdated(ZonedDateTime.now());
+        jiraWorklogDto.setId("123");
+        jiraWorklogDto.setIssueId("444");
+        jiraWorklogDto.setTimeSpentSeconds(202L);
+        jiraWorklogDto.setUpdateAuthor(userDtoFirst());
+        jiraWorklogDto.setUpdated(ZonedDateTime.now());
         Timestamp timestamp = Timestamp.valueOf(jiraWorklogDto.getUpdated().toLocalDateTime());
         return jiraWorklogDto;
     }
 
-    private JiraUserDto getUserDtoFirst(){
+    private JiraUserDto userDtoFirst(){
         JiraUserDto jiraUserDtoFirst = new JiraUserDto();
-            jiraUserDtoFirst.setSelf("Test-self_test-1");
-            jiraUserDtoFirst.setKey("Key-Test-1");
-            jiraUserDtoFirst.setName("Name-Test-1");
-            jiraUserDtoFirst.setEmailAddress("test1@mail.ru");
-            jiraUserDtoFirst.setDisplayName("DisplayName");
-            jiraUserDtoFirst.setActive(true);
-            jiraUserDtoFirst.setDeleted(false);
-            jiraUserDtoFirst.setTimeZone("6+");
-            jiraUserDtoFirst.setLocale("RU");
+        jiraUserDtoFirst.setSelf("Test-self_test-1");
+        jiraUserDtoFirst.setKey("Key-Test-1");
+        jiraUserDtoFirst.setName("Name-Test-1");
+        jiraUserDtoFirst.setEmailAddress("test1@mail.ru");
+        jiraUserDtoFirst.setDisplayName("DisplayName");
+        jiraUserDtoFirst.setActive(true);
+        jiraUserDtoFirst.setDeleted(false);
+        jiraUserDtoFirst.setTimeZone("6+");
+        jiraUserDtoFirst.setLocale("RU");
         return jiraUserDtoFirst;
     }
 
+    private TransferContext transferContext() {
+        TransferContext ctx = new TransferContext();
+        ctx.setSource(Source.JIRA_1);
+        ctx.setLogId(1);
+        return ctx;
+    }
 
 }
