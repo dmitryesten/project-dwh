d959295c03c54f1213cecc638480105ed3985b67
andrew <october072017@yandex.ru>
2021-02-06 18:12:23 +0600

    FB-001: Profiles fix.

--- src/test/java/com/sample/leantech/transfer/TransferApplicationTests.java
+++ src/test/java/com/sample/leantech/transfer/TransferApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("tests,default")
+@ActiveProfiles(value = {"tests", "default"})
 class TransferApplicationTests {
 
 	@Test
--- src/test/java/com/sample/leantech/transfer/model/mapper/AbstractMapperTest.java
+++ src/test/java/com/sample/leantech/transfer/model/mapper/AbstractMapperTest.java
@@ -5,7 +5,7 @@ import com.sample.leantech.transfer.model.context.Source;
 import com.sample.leantech.transfer.model.context.TransferContext;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("tests,default")
+@ActiveProfiles(value = {"tests", "default"})
 public abstract class AbstractMapperTest {
 
     JiraResult jiraResult() {
