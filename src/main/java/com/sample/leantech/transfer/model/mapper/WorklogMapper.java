dba0de435bb766881b3a60f4e1b0dc1c31658a79
andrew <october072017@yandex.ru>
2021-02-12 17:32:12 +0600

    FB-001: WorklogMapperTest fix.

--- src/main/java/com/sample/leantech/transfer/model/mapper/WorklogMapper.java
+++ src/main/java/com/sample/leantech/transfer/model/mapper/WorklogMapper.java
@@ -2,6 +2,7 @@ package com.sample.leantech.transfer.model.mapper;
 
 import com.sample.leantech.transfer.model.context.TransferContext;
 import com.sample.leantech.transfer.model.db.Worklog;
+import com.sample.leantech.transfer.model.dto.request.JiraUserDto;
 import com.sample.leantech.transfer.model.dto.request.JiraWorklogDto;
 import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
@@ -22,6 +23,12 @@ public interface WorklogMapper {
 
     @AfterMapping
     default void afterDtoToModel(JiraWorklogDto source, @MappingTarget Worklog target, @Context TransferContext ctx) {
+        JiraUserDto jiraUserDto = source.getUpdateAuthor();
+        if (jiraUserDto != null) {
+            // TODO: keys have non-Integer values ("JIRAUSER10901" format)
+            // TODO: change column type or remove non-Integer part of key
+            target.setUserId(Integer.valueOf(jiraUserDto.getKey()));
+        }
         if (ctx != null) {
             target.setSid(ctx.getSource().getValue());
             target.setLogId(ctx.getLogInfo().getLogId());
--- src/test/java/com/sample/leantech/transfer/model/mapper/WorklogMapperTest.java
+++ src/test/java/com/sample/leantech/transfer/model/mapper/WorklogMapperTest.java
@@ -4,42 +4,50 @@ import com.sample.leantech.transfer.model.context.TransferContext;
 import com.sample.leantech.transfer.model.db.Worklog;
 import com.sample.leantech.transfer.model.dto.request.JiraUserDto;
 import com.sample.leantech.transfer.model.dto.request.JiraWorklogDto;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.sql.Timestamp;
 import java.time.ZonedDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class WorklogMapperTest extends AbstractMapperTest {
 
     @Test
-    public void test(){
-        JiraWorklogDto jiraWorklogDto = worklogDto();
+    public void testDtoToModel() {
+        JiraWorklogDto worklogDto = jiraWorklogDto();
         TransferContext ctx = transferContext();
-        Worklog worklog = WorklogMapper.INSTANCE.dtoToModel(jiraWorklogDto, ctx);
-        Assertions.assertNotNull(worklog);
-        Assertions.assertEquals(jiraWorklogDto.getIssueId(), String.valueOf(worklog.getIssueId()) );
-        Assertions.assertEquals(jiraWorklogDto.getUpdated().toInstant(), worklog.getUpdated().toInstant());
+
+        Worklog worklog = WorklogMapper.INSTANCE.dtoToModel(worklogDto, ctx);
+
+        assertThat(worklog.getId()).isNull();
+        assertThat(worklog.getIssueId()).isEqualTo(Integer.valueOf(worklogDto.getIssueId()));
+        assertThat(worklog.getLogId()).isEqualTo(ctx.getLogInfo().getLogId());
+        assertThat(worklog.getSid()).isEqualTo(ctx.getSource().getValue());
+        assertThat(worklog.getSourceId()).isEqualTo(Integer.valueOf(worklogDto.getId()));
+        assertThat(worklog.getUpdated()).isEqualTo(Timestamp.valueOf(worklogDto.getUpdated().toLocalDateTime()));
+        assertThat(worklog.getTimeSpentSecond()).isEqualTo(worklogDto.getTimeSpentSeconds().intValue());
+        assertThat(worklog.getUsername()).isNull();
+        assertThat(worklog.getUserId()).isEqualTo(Integer.valueOf(worklogDto.getUpdateAuthor().getKey()));
     }
 
-    private JiraWorklogDto worklogDto(){
-        JiraWorklogDto jiraWorklogDto = new JiraWorklogDto();
-        jiraWorklogDto.setId("123");
-        jiraWorklogDto.setIssueId("444");
-        jiraWorklogDto.setTimeSpentSeconds(202L);
-        jiraWorklogDto.setUpdateAuthor(userDtoFirst());
-        jiraWorklogDto.setUpdated(ZonedDateTime.now());
-        Timestamp timestamp = Timestamp.valueOf(jiraWorklogDto.getUpdated().toLocalDateTime());
-        return jiraWorklogDto;
+    private JiraWorklogDto jiraWorklogDto() {
+        JiraWorklogDto dto = new JiraWorklogDto();
+        dto.setId("123");
+        dto.setIssueId("444");
+        dto.setTimeSpentSeconds(202L);
+        dto.setUpdateAuthor(jiraUserDto());
+        dto.setUpdated(ZonedDateTime.now());
+        return dto;
     }
 
-    private JiraUserDto userDtoFirst(){
-        JiraUserDto jiraUserDtoFirst = new JiraUserDto();
-        jiraUserDtoFirst.setKey("Key-Test-1");
-        jiraUserDtoFirst.setName("Name-Test-1");
-        return jiraUserDtoFirst;
+    private JiraUserDto jiraUserDto() {
+        JiraUserDto dto = new JiraUserDto();
+        dto.setKey("10001");
+        dto.setName("Name-Test-1");
+        return dto;
     }
 
 }
